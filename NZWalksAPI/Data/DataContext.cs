fa7b10e16ed2145b3736558a2cf1447c8f9d1773
Ankit Gwari <ankit.gwari@optimusinfo.com>
2024-11-19 18:20:46 +0530

    Add project files.

---
+++ NZWalksAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models;
+
+namespace NZWalksAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+        
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions{ get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
