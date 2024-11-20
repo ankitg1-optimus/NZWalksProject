3ac90a31eb1af3a7e089b476515c36c7d7d4855e
Ankit Gwari <ankit.gwari@optimusinfo.com>
2024-11-20 10:02:09 +0530

    Added controller

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Data;
+using NZWalksAPI.Models;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        public RegionsController(DataContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+
+        public readonly DataContext DbContext;
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = this.DbContext.Regions.ToList();
+            return Ok(regions);
+        }
+    }
+}
