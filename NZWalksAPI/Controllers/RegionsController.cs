588a27d185db7bfe30b07190dc2a83a6a2483748
Ankit Gwari <ankit.gwari@optimusinfo.com>
2024-11-20 14:43:22 +0530

    completed CRUD operations

--- NZWalksAPI/Controllers/RegionsController.cs
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using NZWalksAPI.Data;
 using NZWalksAPI.Models;
+using NZWalksAPI.Models.Domain;
 using NZWalksAPI.Models.DTO;
 
 namespace NZWalksAPI.Controllers
@@ -10,18 +11,18 @@ namespace NZWalksAPI.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
-        public RegionsController(DataContext DbContext)
+        public RegionsController(DataContext dbContext)
         {
-            this.DbContext = DbContext;
+            this.dbContext = dbContext;
         }
 
-        public readonly DataContext DbContext;
+        public readonly DataContext dbContext;
 
         [HttpGet]
         public IActionResult GetAll()
         {
             //domain models
-            var regionsDomain = this.DbContext.Regions.ToList();
+            var regionsDomain = this.dbContext.Regions.ToList();
             //map Domain models to DTOs
             var regionsDTO = new List<RegionDTO>();
 
@@ -42,9 +43,13 @@ namespace NZWalksAPI.Controllers
         [Route("{id:Guid}")]
         public IActionResult GetById([FromRoute] Guid id) 
         {
-            //var region = this.DbContext.Regions.FirstOrDefault(x=> x.Id==id);
+            //var region = this.dbContext.Regions.FirstOrDefault(x=> x.Id==id);
             //GET region Domain Model
-            var regionDomain = this.DbContext.Regions.Find(id);
+            var regionDomain = this.dbContext.Regions.Find(id);
+            if (regionDomain == null) 
+            {
+                return NotFound();
+            }
             //Map domain model to dto
             var regionDTO = new RegionDTO
             {
@@ -55,6 +60,89 @@ namespace NZWalksAPI.Controllers
             };
             //return DTO
             return Ok(regionDTO);
+        }
+
+        [HttpPost]
+        public IActionResult CreateRegion([FromBody]RegionDTO regionDTO) {
+            try
+            {
+                var regionDomain = new Region
+                {
+                    Code = regionDTO.Code,
+                    Name = regionDTO.Name,
+                    RegionImageUrl = regionDTO.RegionImageUrl
+                };
+
+                dbContext.Regions.Add(regionDomain);
+                dbContext.SaveChanges();
+
+                var addedRegion = new RegionDTO
+                {
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                };
+                return CreatedAtAction(nameof(GetById), new {id = regionDomain.Id}, addedRegion);
+            }
+            catch (Exception) {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult UpdateRegion([FromRoute]Guid id, [FromBody]RegionDTO regionDTO ) {
+            try
+            {
+                var regionDomainModel = dbContext.Regions.Find(id);
+                if (regionDomainModel == null) 
+                {
+                    return NotFound();
+                }
+
+                regionDomainModel.Code = regionDTO.Code;
+                regionDomainModel.Name = regionDTO.Name;
+                regionDomainModel.RegionImageUrl = regionDTO.RegionImageUrl;
+                dbContext.SaveChanges();
+
+                var updatedModel = new RegionDTO
+                {
+                    Code = regionDomainModel.Code,
+                    Name = regionDomainModel.Name,
+                    RegionImageUrl = regionDomainModel.RegionImageUrl
+                };
+                return Ok(updatedModel);
+            }
+            catch (Exception) {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult DeleteRegion([FromRoute]Guid id) {
+            try
+            {
+                var regionDomainModel = dbContext.Regions.Find(id);
+                if (regionDomainModel == null) 
+                {
+                    return NotFound();
+                }
+
+                dbContext.Regions.Remove(regionDomainModel);
+                dbContext.SaveChanges();
+
+                var deletedModel = new RegionDTO
+                {
+                    Code = regionDomainModel.Code,
+                    Name = regionDomainModel.Name,
+                    RegionImageUrl = regionDomainModel.RegionImageUrl
+                };
+                return Ok(deletedModel);
+            }
+            catch (Exception) {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
